a62078d5e8fc847fd0b87539880e9f8849625dac
satyapalkhakhal <khakhalsatyapal@gmail.com>
2024-09-02 02:25:26 +0530

    blog project

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { Children, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import About from './About.jsx'
+import NewBlog from './components/NewBlog/NewBlog.jsx'
+const router =createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<App/>} />
+      <Route path='about' element={<About/>} />
+      <Route path='new' element={<NewBlog/>} />
+    </Route>
+  )
+)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </StrictMode>,
+)
